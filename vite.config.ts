e2ca79d94162b041563157ed2cbfce9c19fe806f
Cristina Restrepo <crispe1586@gmail.com>
2025-05-28 20:23:20 +0200

    add build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ exclude: ['**/*.stories.ts', '**/*.test.ts']})
+  ],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'ui',
+      fileName: (format) => `ui.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    }
+  },
 })
