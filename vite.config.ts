89488cd347e0b1912aa5459ff4d1ce32f911d380
Cristina Restrepo <crispe1586@gmail.com>
2025-05-28 20:32:30 +0200

    add rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { peerDependencies } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    dts({ exclude: ['**/*.stories.ts', '**/*.test.ts']})
+    dts({ exclude: ['**/*.stories.ts', '**/*.test.ts'] })
   ],
   build: {
     lib: {
@@ -14,6 +15,15 @@ export default defineConfig({
       name: 'ui',
       fileName: (format) => `ui.${format}.js`,
       formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies),
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        }
+      }
     }
-  },
+  }
 })
