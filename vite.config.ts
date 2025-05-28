381224b408c55ccfcaf08fbd92e35817701c89ad
Cristina Restrepo <crispe1586@gmail.com>
2025-05-29 00:18:50 +0200

    add vite tailwind plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import tailwindcss from '@tailwindcss/vite'
 import { peerDependencies } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     dts({ exclude: ['**/*.stories.ts', '**/*.test.ts'] })
   ],
   build: { 
