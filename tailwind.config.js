46a8e7ac644b4adc2cd6c0062ab842554164681a
Cristina Restrepo <crispe1586@gmail.com>
2025-05-30 00:04:14 +0200

    add variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
     content: [
-        './src/**/*.{js,ts,jsx,tsx}',
+        './src/**/*.{js,ts,jsx,tsx}'
     ],
     darkMode: 'class',
     important: '.ui',
     plugins: [],
     prefix: 'ui:',
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif', 'system-ui'],
+                expressive: ['Bebas Neue', 'times', 'system-ui'],
+            },
+            colors: {
+                background: 'var(--color-background)',
+                foreground: 'var(--color-foreground)',
+                link: 'var(--color-link)',
+                'link-hover': 'var(--color-link-hover)',
+                'button-foreground': 'var(--color-button-border)',
+                'button-foreground-hover': 'var(--color-button-border-hover)',
+            },
+        },
     },
 }
